b9a555de39e40740452810bad5e26d2ba49aa818
rainybreeze777 <psting3456@gmail.com>
2015-12-24 19:46:41 -0500

    Modify SpriteInfo json sheet to use correct format
    Also adds a stub class to get all information regarding tiles.

---
+++ Assets/Scripts/Util/TileInfoFetcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class TileInfoFetcher {
+
+	private string spriteJsonSheet = null;
+	private static TileInfoFetcher fetcher = null;
+
+	private TileInfoFetcher () {
+		TextAsset jsonText = Resources.Load("SpritesInfo") as TextAsset;
+		spriteJsonSheet = JSON.Parse(jsonText.text);
+	}
+
+	public TileInfoFetcher GetInstance() {
+		if (fetcher == null)
+			fetcher = new TileInfoFetcher();
+
+		return fetcher;
+	}
+}
