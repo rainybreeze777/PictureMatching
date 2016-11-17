rainybreeze777/PictureMatching

commit cee6bf603d796a603bb33f7a8997050b8d0a89a8

Fix bug where elemGathered won't reset after battle

Assets/Scripts/Interfaces/IComboModel.cs
@@ -8,7 +8,7 @@ public interface IComboModel {
 
     List<int> GetCancelSeq();
 
-    void ClearCancelSequence();
+    void ResetBattleStatus();
 
     Signal<int> CancelAddedSignal { get; }
 

Assets/Scripts/Mediators/ComboViewMediator.cs
@@ -20,7 +20,7 @@ public class ComboViewMediator : Mediator {
         view.Init();
 
         comboModel.CancelAddedSignal.AddListener(OnTileCancelled);
-        resetActiveStateSignal.AddListener(ClearCancelSequence);
+        resetActiveStateSignal.AddListener(ResetBattle);
         enemySeqGenSignal.AddListener(OnEnemySeqGeneration);
     }
 
@@ -32,8 +32,8 @@ public class ComboViewMediator : Mediator {
         view.ConstructNewEnemySequence(enemyModel.GetPrevGeneratedSequence(), enemyModel.GetPrevSequenceMask());
     }
 
-    public void ClearCancelSequence() {
-        comboModel.ClearCancelSequence();
+    public void ResetBattle() {
+        comboModel.ResetBattleStatus();
         view.Reset();
     }
 }

Assets/Scripts/Models/ComboModel.cs
@@ -68,8 +68,16 @@ public class ComboModel : IComboModel {
         return cancelSequence;
     }
 
-    public void ClearCancelSequence() {
+    public void ResetBattleStatus() {
         cancelSequence.Clear();
+
+        List<EElements> elems = new List<EElements>(elemGathered.Keys);
+        foreach(EElements e in elems) {
+            elemGathered[e] = 0;
+            elemGatherUpdatedSignal.Dispatch(e, 0);
+        }
+
+        RefreshSkillPrepStatus();
     }
 
     public void DeductComboElems(int comboId) {

commit 2090351597b4862410023abd086fd6a98cacf11a

Fix bug where essence is being reset after every round
This bug was accidentally introduced in commit cee6bf603d796a603bb33f7a8997050b8d0a89a8

Assets/Scripts/Interfaces/IComboModel.cs
@@ -14,5 +14,7 @@ public interface IComboModel {
 
     void DeductComboElems(int comboId);
 
+    void ClearCancelSequence();
+
     void RefreshEquippedCombo();
 }

Assets/Scripts/Mediators/ComboViewMediator.cs
@@ -20,7 +20,7 @@ public class ComboViewMediator : Mediator {
         view.Init();
 
         comboModel.CancelAddedSignal.AddListener(OnTileCancelled);
-        resetActiveStateSignal.AddListener(ResetBattle);
+        resetActiveStateSignal.AddListener(ResetRound);
         enemySeqGenSignal.AddListener(OnEnemySeqGeneration);
     }
 
@@ -32,8 +32,8 @@ public class ComboViewMediator : Mediator {
         view.ConstructNewEnemySequence(enemyModel.GetPrevGeneratedSequence(), enemyModel.GetPrevSequenceMask());
     }
 
-    public void ResetBattle() {
-        comboModel.ResetBattleStatus();
+    public void ResetRound() {
+        comboModel.ClearCancelSequence();
         view.Reset();
     }
 }

Assets/Scripts/Mediators/GameViewMediator.cs
@@ -16,8 +16,6 @@ public class GameViewMediator : Mediator {
     [Inject]
     public BattleResultUpdatedSignal battleResultUpdatedSignal { get; set; }
     [Inject]
-    public ResetBattleSignal resetBattleSignal{ get; set; }
-    [Inject]
     public BoardIsEmptySignal boardIsEmptySignal { get; set; }
     [Inject]
     public ResetActiveStateSignal resetActiveStateSignal { get; set; }

Assets/Scripts/Models/ComboModel.cs
@@ -16,6 +16,8 @@ public class ComboModel : IComboModel {
     public IInBattleStatus playerStatus { get; set; }
     [Inject]
     public PlayerEquipComboUpdatedSignal playerEquipComboUpdatedSignal { get; set; }
+    [Inject]
+    public ResetBattleSignal resetBattleSignal{ get; set; }
     public Signal<int> cancelAddedSignal = new Signal<int>();
     public Signal<int> CancelAddedSignal 
     {
@@ -50,6 +52,7 @@ public class ComboModel : IComboModel {
         equippedComboList = playerStatus.GetEquippedCombos();
         comboExecFinishedSignal.AddListener(DeductComboElems);
         playerEquipComboUpdatedSignal.AddListener(RefreshEquippedCombo);
+        resetBattleSignal.AddListener(ResetBattleStatus);
     }
 
     public void AddToCancelSequence(int tileNumber) {
@@ -68,9 +71,11 @@ public class ComboModel : IComboModel {
         return cancelSequence;
     }
 
-    public void ResetBattleStatus() {
+    public void ClearCancelSequence() {
         cancelSequence.Clear();
+    }
 
+    public void ResetBattleStatus() {
         List<EElements> elems = new List<EElements>(elemGathered.Keys);
         foreach(EElements e in elems) {
             elemGathered[e] = 0;